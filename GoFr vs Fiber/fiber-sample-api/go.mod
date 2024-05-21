c0518fe981e278cd030d8b78060305dc97675064
umang01-hash <mundhraumang.02@gmail.com>
2024-05-21 15:45:06 +0530

    inital commit for GoFr vs Fiber article

---
+++ GoFr vs Fiber/fiber-sample-api/go.mod
@@ -0,0 +1,22 @@
+module github.com/fiber-api
+
+go 1.22.0
+
+require (
+	github.com/go-sql-driver/mysql v1.8.1
+	github.com/gofiber/fiber/v3 v3.0.0-beta.2
+)
+
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/andybalholm/brotli v1.1.0 // indirect
+	github.com/gofiber/utils/v2 v2.0.0-beta.4 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/klauspost/compress v1.17.6 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.52.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/sys v0.17.0 // indirect
+)
---
+++ GoFr vs Fiber/fiber-sample-api/main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+
+	"github.com/gofiber/fiber/v3"
+)
+
+type Product struct {
+	ID    int     `json:"id"`
+	Name  string  `json:"name"`
+	Price float64 `json:"price"`
+}
+
+var db *sql.DB
+
+const (
+	createQuery = "INSERT INTO products (id, name, price) VALUES (?, ?, ?)"
+	selectQuery = "SELECT id, name, price FROM products WHERE id = ?"
+)
+
+func initDB() {
+	var err error
+
+	connectionString := "root:password@tcp(localhost:2001)/test?charset=utf8&parseTime=True&loc=Local"
+	db, err = sql.Open("mysql", connectionString)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	// Initialize a new Fiber app
+	app := fiber.New()
+
+	initDB()
+
+	defer db.Close()
+
+	app.Post("/product", func(c fiber.Ctx) error {
+
+		var product Product
+
+		err := c.Bind().Body(&product)
+		if err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": "cannot parse JSON",
+			})
+		}
+
+		result, err := db.Exec(createQuery, product.ID, product.Name, product.Price)
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": fmt.Sprintf("cannot create product : %v", err),
+			})
+		}
+
+		_, err = result.LastInsertId()
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": "cannot retrieve product ID",
+			})
+		}
+
+		return c.Status(fiber.StatusCreated).JSON(product)
+	})
+
+	app.Get("/product/:id", func(c fiber.Ctx) error {
+		var product Product
+
+		id := c.Params("id")
+
+		err := db.QueryRow(selectQuery, id).Scan(&product.ID, &product.Name, &product.Price)
+		if errors.Is(err, sql.ErrNoRows) {
+			return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+				"error": "product not found",
+			})
+		} else if err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": "cannot retrieve product",
+			})
+		}
+
+		return c.JSON(product)
+	})
+
+	// Start the server on port 3000
+	log.Fatal(app.Listen(":3000"))
+}
